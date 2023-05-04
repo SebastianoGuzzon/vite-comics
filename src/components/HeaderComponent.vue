7cd71fa429b9ce5d4ccec970aa534808c4abee7a
Sebastiano <sebyguzz@icloud.com>
2023-05-04 08:07:48 +0200

    Altre modifiche su codice Vue

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,5 @@
-<!-- Creazione componente Header -->
 <template>
   <div>
-    <!-- Creazione  -->
     <div class="d-flex container justify-content-between align-items-center py-4">
       <img src="../../public/img/dc-logo.png" alt="">
       <ul class="d-flex gap-4 w-75 text-uppercase fw-bold">
@@ -26,4 +24,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+  cursor: pointer;
+
+  &::after {
+    display: block;
+    content: '';
+    width: 100%;
+    height: 235%;
+    border-bottom: 4px solid transparent;
+  }
+
+  &:hover,
+  &.active {
+    color: #2b82f9;
+  }
+
+  &:hover::after,
+  &.active::after {
+    border-bottom: 4px solid #2b82f9;
+  }
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,9 +1,6 @@
-<!-- Creazione componente Main -->
 <template>
   <main>
     <div class="bg-dark p-4">
-
-      <!-- dati della card  -->
       <div class="container py-5 d-flex flex-wrap gap-5">
         <CardsComponent v-for="(card, index) in data" :key="index" :data="card" />
       </div>
@@ -12,8 +9,6 @@
       <div class="container">
         <ul class="d-flex align-items-center p-5 text-white text-uppercase gap-3">
           <li>
-            <!-- carico immagini -->
-
             <img src="../../public/img/buy-comics-digital-comics.png" alt="" class="pe-3">
             <span>Digital comics</span>
           </li>
@@ -38,10 +33,8 @@
     </div>
   </main>
 </template>
-     
-<!-- dati della card  -->
-<script>
 
+<script>
 import CardsComponent from './CardsComponent.vue';
 import { comics } from '../data/dc-comics';
 
@@ -57,4 +50,12 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+  width: 30%;
+
+  & img {
+    width: 30%;
+  }
+}
+</style>
